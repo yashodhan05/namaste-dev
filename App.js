c641f3d225dfbd3d1ad2a1c2728fd64317d58236
Yashodhan Kulkarni <yashodhankulkarni88@gmail.com>
2026-05-14 01:54:10 +0530

    Basics

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div", { id: "parent" },
+   [ React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
+    ),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
+    )
+   ]
+);
+
+root.render(parent);
