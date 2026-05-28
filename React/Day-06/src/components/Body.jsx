47814dfa79dbac01d4b6a666a5380930712e2f92
Yashodhan Kulkarni <yashodhankulkarni88@gmail.com>
2026-05-28 23:25:04 +0530

    adding cors proxy

--- React/Day-06/src/components/Body.jsx
+++ React/Day-06/src/components/Body.jsx
@@ -14,7 +14,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5165691&lng=73.8036752&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5165691&lng=73.8036752&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     );
     const jsonData = await data.json();
     setRestaurants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
